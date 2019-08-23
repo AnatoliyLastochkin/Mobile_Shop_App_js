539ccbf3f0d0c624ab996930c6eada4d0fe73ad6
Anatoliy <AnatoliyLastochkin@gmail.com>
2019-08-23 14:55:00 +0200

    initComponent was implemented

---
+++ js/Components/PhonesPage.js
@@ -0,0 +1,41 @@
+import PhonesCatalog from './PhonesCatalog.js';
+import ShoppingCart from './ShoppingCart.js';
+import Filter from './Filter.js';
+export default class PhonesPage {
+  constructor(element) {
+    this.element = element;
+
+    this.render();
+
+    this.initComponent(PhonesCatalog);
+    this.initComponent(ShoppingCart);
+    this.initComponent(Filter);
+  }
+
+  initComponent(Constructor) {
+    new Constructor(this.element.querySelector(`[data-component="${Constructor.name}"]`));
+  }
+
+  render() {
+    this.element.innerHTML = `
+      <div class="row">
+  
+        <!--Sidebar-->
+        <div class="col-md-2">
+          <section data-component="Filter">
+            
+          </section>
+  
+          <section data-component="ShoppingCart">
+            
+          </section>
+        </div>
+  
+        <!--Main content-->
+        <div class="col-md-10" data-component="PhonesCatalog">
+  
+        </div>
+      </div>  
+    `;
+  }
+}
